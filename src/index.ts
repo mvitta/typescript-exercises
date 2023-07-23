c49158dd567e366aa06b4ec7c1cf74c0c5511df4
Mike <mikesystem@hotmail.com>
2023-07-22 19:46:10 -0500

    ej 1 leetcode

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import { Product } from './modules/product.model';
 import { allProducts } from './modules/product.model';
 import express from 'express';
+import { log, table } from 'console';
 
 (() => {
   const myProducts: Product[] = [];
@@ -123,29 +124,80 @@ import express from 'express';
     image: 'link-de-la-imagen',
   });
 
-  showProducts();
+  //-------------------------LEETCODE---------------------------------------------
+  function removeElement(nums: number[], val: number): number {
+    const MAX_LENGTH = 100;
+    const MIN_LENGTH = 0;
+    if (!(nums.length >= MIN_LENGTH && nums.length <= MAX_LENGTH)) {
+      throw Error('Error Length');
+    }
+    let i = 0;
+    for (const num of nums) {
+      if (num != val) {
+        nums[i++] = num;
+      }
+    }
+    return i;
+  }
+  //-----------------------------------------------------------------------------
+  // return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+  function strStr(haystack: string, needle: string): number {
+    return haystack.indexOf(needle, 0);
+  }
 
-  //----------------------------------------------------
-  let sum: number = 0;
-  myProducts.forEach((item) => {
-    const { price } = item;
-    sum += price;
-  });
-  console.log({ sum });
+  //-----------------------------------------------------------------------------------
 
-  const server = (): void => {
-    const app = express();
-    const port: number = 3000;
+  function twoSum(nums: number[], target: number): number[] {
+    let aux: number[] = [];
+    let sum = 0;
+    let exit = false;
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        sum += nums[i] + nums[j];
+        if (sum === target) {
+          exit = true;
+          aux = [i, j];
+          break;
+        }
+        sum = 0;
+      }
+      if (exit) break;
+    }
+    return aux;
+  }
+  //-------------------------------------------------------------------------------
+  function longestCommonPrefix(strs: string[]): string {
+    // comparar caracter con cada palabra eh ir concatenando
+    let ch = '';
+    let exit = false;
+    for (let i = 0; i < strs[0].length; i++) {
+      ch += strs[0][i];
+      console.log(ch);
+      for (const str of strs) {
+        const r = str.startsWith(ch);
+        console.log({ r });
+        if (!r) {
+          exit = true;
+          ch = ch.slice(0, -1);
+          break;
+        }
+      }
+      if (exit) break;
+    }
 
-    app.get('/', (req, res) => {
-      res.send(`Hello word, ${new Date()}`);
-      res.status(200);
-    });
+    return ch;
+  }
 
-    app.listen(port, () => {
-      console.log(`listening http://localhost:${port}/`);
-    });
-  };
+  //------------------------------------------------------------------------------
+  function plusOne(digits: number[]): number[] {
+    return (BigInt(digits.toString().split(',').join('')) + BigInt(1))
+      .toString()
+      .split('')
+      .map((n) => Number(n));
+  }
 
-  //fin
+  const output = plusOne([
+    6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3,
+  ]);
+  console.log({ output });
 })();
